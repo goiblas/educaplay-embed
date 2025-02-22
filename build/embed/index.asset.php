9d47239332556738bb9d72a034462a8e88817f5a
Jesús Olazagoitia <jesus.olazagoitia@gmail.com>
2025-02-22 11:05:44 +0100

    update css

--- build/embed/index.asset.php
+++ build/embed/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '59c2f08b6b1ee05c3b89');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'c7298948ea3192f721d2');
